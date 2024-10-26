d58c4bd9ced574ab14341b9e52f3ef6e55cecd65
Paul Schifferer <paul@schifferers.net>
2024-10-25 22:40:14 -0700

    Fix test

--- util/env_test.go
+++ util/env_test.go
@@ -6,30 +6,30 @@ import (
 )
 
 func TestGetEnv(t *testing.T) {
-	os.Setenv("TEST", "value")
-	value := GetEnv("TEST", "default")
+	os.Setenv("TGE", "value")
+	value := GetEnv("TGE", "default")
 	if value != "value" {
 		t.Fail()
 	}
 }
 
 func TestGetEnvDefault(t *testing.T) {
-	value := GetEnv("TEST", "default")
+	value := GetEnv("TGED", "default")
 	if value != "default" {
 		t.Fail()
 	}
 }
 
 func TestGetEnvInt(t *testing.T) {
-	os.Setenv("TEST", "1234")
-	value := GetEnvInt("TEST", 5678)
+	os.Setenv("TGEI", "1234")
+	value := GetEnvInt("TGEI", 5678)
 	if value != 1234 {
 		t.Fail()
 	}
 }
 
 func TestGetEnvIntDefault(t *testing.T) {
-	value := GetEnvInt("TEST", 5678)
+	value := GetEnvInt("TGEID", 5678)
 	if value != 5678 {
 		t.Fail()
 	}
