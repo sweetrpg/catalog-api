7990992fd295cb774fc134768138169ae422f10d
Paul Schifferer <paul@schifferers.net>
2024-10-25 20:59:57 -0700

    Tracing

--- cmd/catalog-api/main.go
+++ cmd/catalog-api/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"github.com/getsentry/sentry-go"
 	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/penglongli/gin-metrics/ginmetrics"
 	"github.com/sweetrpg/catalog-api/constants"
 	"github.com/sweetrpg/catalog-api/database"
 	"github.com/sweetrpg/catalog-api/logging"
 	"github.com/sweetrpg/catalog-api/server"
+	"github.com/sweetrpg/catalog-api/tracing"
 	"github.com/sweetrpg/catalog-api/util"
-	"log"
-	"os"
-	"time"
 )
 
 func main() {
@@ -29,7 +32,7 @@ func main() {
 			// Release: "my-project-name@1.0.0",
 		})
 		defer func() {
-			log.Print("Flusing Sentry...")
+			log.Print("Flushing Sentry...")
 			sentry.Flush(2 * time.Second)
 		}()
 	}
@@ -37,11 +40,21 @@ func main() {
 	r := gin.Default()
 	// r.LoadHTMLGlob("tmpl/*")
 
+	tracing.SetupTracing()
+	defer tracing.TeardownTracing()
+
+	// get global Monitor object
+	m := ginmetrics.GetMonitor()
+	m.SetMetricPath("/metrics")
+	m.SetSlowTime(10)
+	m.SetDuration([]float64{0.1, 0.3, 1.2, 5, 10})
+	m.Use(r)
+
 	var cache persistence.CacheStore
 	redisHost, found := os.LookupEnv(constants.REDIS_HOST)
 	if found {
 		redisPort := util.GetEnv(constants.REDIS_PORT, "6379")
-		cache = persistence.NewRedisCache(fmt.Sprintf("%s:%s", redisHost, redisPort), "", time.Hour)
+		cache = persistence.NewRedisCache(fmt.Sprintf("%s:%s", redisHost, redisPort), constants.REDIS_PASS, time.Hour)
 	} else {
 		cache = persistence.NewInMemoryStore(time.Hour)
 	}
@@ -50,15 +63,6 @@ func main() {
 	defer database.TeardownDatabase()
 
 	server.SetupHandlers(r, cache)
-
-	// http.HandleFunc("/view/", server.MakeHandler(server.ViewHandler))
-	// r.GET("/view/:name", server.ViewHandler)
-	// http.HandleFunc("/edit/", server.MakeHandler(server.EditHandler))
-	// http.HandleFunc("/save/", server.MakeHandler(server.SaveHandler))
-	// http.HandleFunc("/", server.MainHandler)
-	// r.GET("/", server.MainHandler)
-
-	// log.Fatal(http.ListenAndServe(":8080" /*os.Getenv("ADDR")*/, nil))
 
 	r.Run(util.GetEnv(constants.BIND_ADDRESS, ":8000"))
 }
