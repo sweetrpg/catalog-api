94cdfbff3a1ac1a028bd6d4e1089cf68d40b76c9
Paul Schifferer <paul@schifferers.net>
2026-09-06 20:38:23 -0700

    style(server): sort authz-client import below catalog-api in write handlers

--- server/handlers.go
+++ server/handlers.go
@@ -3,8 +3,8 @@ package server
 import (
 	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
-	"github.com/sweetrpg/catalog-api/assets"
 	"github.com/sweetrpg/authz-client.go/authz"
+	"github.com/sweetrpg/catalog-api/assets"
 	"github.com/sweetrpg/catalog-api/cachettl"
 	"github.com/sweetrpg/catalog-api/editsession"
 	"github.com/sweetrpg/catalog-api/internal/events"
